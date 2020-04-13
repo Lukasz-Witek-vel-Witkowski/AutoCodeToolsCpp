fabe7aaa1b88a9320c59dc13e79bb593c932f3fa
Lukasz Witek vel Witkowski <uwlwitkowski@gmail.com>
2020-04-13 16:28:25 +0200

    add new class ModuleClass

--- moduleclass.cpp
+++ moduleclass.cpp
@@ -1,1 +1,26 @@
 #include "moduleclass.hpp"
+
+ModuleClass::ModuleClass(){
+
+}
+std::string ModuleClass::StartClass(){
+    return "class "+Nameclass+"\n{\n";
+}
+std::string ModuleClass::StopClass(){
+    return "}; //End class "+Nameclass+"\n";
+}
+void ModuleClass::AddNameClas(std::string name){
+    Nameclass = name;
+}
+std::string ModuleClass::ConstructorClass(bool b){
+    if(b)
+    return Nameclass+"();\n";
+    return Nameclass+"::"+Nameclass+"(){\n}//Constructor "+Nameclass+"\n";
+}
+std::string ModuleClass::DestructorClass(bool b){
+    return "~"+Nameclass+"();\n";
+    return Nameclass+"::~"+Nameclass+"(){\n}//Destruktor "+Nameclass+"\n";
+}
+ModuleClass::~ModuleClass(){
+
+}
