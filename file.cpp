59e3e418b5b11352319754d4425d81deec64305d
Lukasz Witek vel Witkowski <uwlwitkowski@gmail.com>
2020-04-13 10:17:00 +0200

    Add new Method 'Creative_matrix' & 'Clear_empty_matrix_value' to class File

--- file.cpp
+++ file.cpp
@@ -6,6 +6,22 @@ void File::creative_matrix(unsigned int value){
     if(value>0){
         V_data.resize(value);
     }
+    for(auto x: V_data){
+        x = "";
+    }
+}
+void File::Clear_empty_matrix_value(){
+    int size_empty =0;
+    for(auto x:V_data){
+        if(x=="") size_empty++;
+    }
+    if(size_empty>1){
+        for(int i =0; i<V_data.size(); i++){
+            if(V_data[i]==""&&size_empty>1){
+                V_data.erase(V_data.begin()+i);
+            }
+        }
+    }
 }
 File::~File(){
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "file.hpp"
 int main(int a_size, char** a_tab){
+    File f;
     return 0;
 }
